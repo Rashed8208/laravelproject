490057a3686093c7d47878faeceae5ed9dafee4f
hsn771 <hr492785@gmail.com>
2025-10-04 09:35:15 +0600

    dis

--- resources/views/customer/auth/customer_register.blade.php
+++ resources/views/customer/auth/customer_register.blade.php
@@ -34,7 +34,7 @@
                          <div class="form-group row mb-3">
                             <label for="phone" class="col-md-4 col-form-label text-md-right">Phone</label>
                             <div class="col-md-6">
-                                <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
                                 @error('phone')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -45,8 +45,19 @@
                         <div class="form-group row mb-3">
                             <label for="address" class="col-md-4 col-form-label text-md-right">Address</label>
                             <div class="col-md-6">
-                                <input id="address" type="address" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
                                 @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                         <div class="form-group row mb-3">
+                            <label for="district" class="col-md-4 col-form-label text-md-right">District</label>
+                            <div class="col-md-6">
+                                <input id="district" type="text" class="form-control @error('district') is-invalid @enderror" name="city" value="{{ old('district') }}" required autocomplete="district">
+                                @error('district')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -56,19 +67,8 @@
                          <div class="form-group row mb-3">
                             <label for="city" class="col-md-4 col-form-label text-md-right">City</label>
                             <div class="col-md-6">
-                                <input id="city" type="city" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city">
+                                <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city">
                                 @error('city')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-group row mb-3">
-                            <label for="district" class="col-md-4 col-form-label text-md-right">District</label>
-                            <div class="col-md-6">
-                                <input id="district" type="district" class="form-control @error('district') is-invalid @enderror" name="city" value="{{ old('district') }}" required autocomplete="district">
-                                @error('district')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -78,7 +78,7 @@
                         <div class="form-group row mb-3">
                             <label for="post_code" class="col-md-4 col-form-label text-md-right">Post_code</label>
                             <div class="col-md-6">
-                                <input id="post_code" type="post_code" class="form-control @error('post_code') is-invalid @enderror" name="post_code" value="{{ old('post_code') }}" required autocomplete="post_code">
+                                <input id="post_code" type="text" class="form-control @error('post_code') is-invalid @enderror" name="post_code" value="{{ old('post_code') }}" required autocomplete="post_code">
                                 @error('post_code')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -89,7 +89,7 @@
                         <div class="form-group row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="text" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -100,7 +100,7 @@
                         <div class="form-group row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
                         <div class="form-group row mb-0">
